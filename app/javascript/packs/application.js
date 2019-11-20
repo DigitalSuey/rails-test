9274f7cf2b3f7db0460726fc3b5a48e14bfe4952
Paulo Pao <paulo@redant.com.au>
2019-11-20 11:43:08 +1100

    Add new react components

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 /* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
@@ -7,7 +10,6 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -15,7 +17,6 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
 // Support component names relative to this directory:
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,27 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root 'static#index'
 
   namespace :v1, defaults: { format: 'json' } do
     get 'things', to: 'things#index'
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles do
-    resources :comments
-  end
-
-  root 'welcome#index'
 end
+
+# Rails.application.routes.draw do
+#   root 'static#index'
+#
+#   # get 'welcome/index'
+#
+#   namespace :v1, defaults: { format: 'json' } do
+#     get 'things', to: 'things#index'
+#   end
+#
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   resources :articles do
+#     resources :comments
+#   end
+#
+#   # Comment other root declarations in the file and uncomment the following for
+#   # the rails only app:
+#   #
+#   # root 'welcome#index'
+# end
